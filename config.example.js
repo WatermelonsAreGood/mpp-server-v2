7bbbd68a45c7c41382b4bcd58e32f572d95566be
yourfriend <81387799+yourfriendoss@users.noreply.github.com>
2023-01-02 15:30:54 +0200

    The example config.js is incorrect.

--- config.example.js
+++ config.example.js
@@ -1,18 +1,20 @@
-export const port = "8080";
-export const motd = "You agree to read this message.";
-export const salt = "boppity";
-export const defaultUsername = "Anonymous";
-export const adminpass = "adminpass";
-export const xforwardedtrust = false;
-export const defaultRoomSettings = {
-    color: "#3b5054",
-    color2: "#001014",
-    crownsolo: false,
-    visible: true
-};
-export const defaultLobbySettings = {
-    color: "#FFA500",
-    color2: "#000000",
-    crownsolo: false,
-    visible: true
-};
+export default {
+    port: "8080",
+    motd: "You agree to read this message.",
+    salt: "boppity",
+    defaultUsername: "Anonymous",
+    adminpass: "adminpass",
+    xforwardedtrust: false,
+    defaultRoomSettings: {
+        color: "#3b5054",
+        color2: "#001014",
+        crownsolo: false,
+        visible: true
+    },
+    defaultLobbySettings: {
+        color: "#FFA500",
+        color2: "#000000",
+        crownsolo: false,
+        visible: true
+    }
+}
